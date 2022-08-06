93013c3b39ca03233a0df467a07d7890d1f64aed
guillaumederybel <vodz4r@gmail.com>
2022-08-06 11:11:09 -0400

    Retrieve scoreboard  title

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import cv2
 import pytesseract
 from PIL import Image
+from pytesseract import Output
 
-print(pytesseract.image_to_string(Image.open('images/test.png')))
+def qualified_title(image):
+    """This function will retreive the number of
+    qualified people for the next round."""
+
+    title_top = 40
+    title_height = 180
+    title_left = 550
+    title_lenght = 1370
+
+    cropped_title = original_image[title_top:title_height, title_left:title_lenght]
+    gray_title = cv2.cvtColor(cropped_title, cv2.COLOR_BGR2GRAY)
+    title = Image.fromarray(gray_title)
+
+    text = pytesseract.image_to_string(title)
+
+    return text
+
+original_image = cv2.imread('images/fgafter.png')
+qualified = qualified_title(original_image)
+print(qualified)
